dc9a185ae2a250efae19ce020312ad5d621c4762
Altanbagana <altanbagana@protonmail.com>
2023-11-16 14:03:13 +0800

    up to date

---
+++ api/src/main.rs
@@ -0,0 +1,20 @@
+use futures::{StreamExt, FutureExt};
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let routes = warp::path("echo")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            ws.on_upgrade(|websocket| {
+                let (tx, rx) = websocket.split();
+                rx.forward(tx).map(|result| {
+                    if let Err(e) = result {
+                        eprintln!("websocket error {:?}", e);
+                    }
+                })
+            })
+        });
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
